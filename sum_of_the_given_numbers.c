9c77743e3c5361dbe4a6694ba4f74f69679dc066
Leelaprasanthi <98008432+Leelaprasanthi@users.noreply.github.com>
2022-06-30 00:51:00 +0530

    #include<stdio.h> int main() {     int i,n,arr[100],brr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&arr[i],&brr[i]);     }     for(i=0;i<n;i++)     {         printf("%d ",arr[i]+brr[i]);     }     return 0; }

--- sum_of_the_given_numbers.c
+++ sum_of_the_given_numbers.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
+    int i,n,arr[100],brr[100];
     scanf("%d",&n);
-    int a[n],b[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d%d",&a[i],&b[i]);
+        scanf("%d%d",&arr[i],&brr[i]);
     }
     for(i=0;i<n;i++)
     {
         printf("%d
-",a[i]+b[i]);
+",arr[i]+brr[i]);
     }
     return 0;
 }
