c2b2b3693688944c17c9ec2c24b056d92795f000
Leelaprasanthi <98008432+Leelaprasanthi@users.noreply.github.com>
2022-06-30 20:38:48 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,sum=0,odd;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if( arr[i]%2)         {             odd=arr[i];         }     }     printf("%d",odd); }

--- last_odd_number_in_an_array.c
+++ last_odd_number_in_an_array.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[100],i,k=0;
+    int arr[100],n,i,sum=0,odd;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -9,10 +9,10 @@ int main()
     }
     for(i=0;i<n;i++)
     {
-        if(arr[i]%2==1)
+        if( arr[i]%2)
         {
-            k=arr[i];
+            odd=arr[i];
         }
     }
-    printf("%d",k);
+    printf("%d",odd);
 }
